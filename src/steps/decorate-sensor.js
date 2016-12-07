fa4dabae8b77981d9c0b820fc9aed9ae6e049965
Manuel Serra <nitroin@hotmail.com>
2016-12-07 13:28:38 +0100

    Added logs

--- src/steps/decorate-sensor.js
+++ src/steps/decorate-sensor.js
@@ -1,9 +1,15 @@
+import log from "services/logger";
 import {upsertSensor} from "services/mongo-db";
 
 export async function decorateSensor(sensorId, measurements) {
 
     const measurementTypes = measurements.map(measurement => {
         return measurement.type;
+    });
+
+    log.info({
+        sensorId,
+        measurementTypes
     });
 
     await upsertSensor(sensorId, {
